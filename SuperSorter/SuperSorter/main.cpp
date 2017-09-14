a0f6eefa18db05245b44592f89189ef8e06592d5
Danyalo <danylolab@gmail.com>
2017-09-14 14:00:42 +0300

    Started a project
    Addes Visual Studio project, wrote the basic main.cpp with array input

---
+++ SuperSorter/SuperSorter/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	cout << "Please enter the number of elemens in array: ";
+	int n;
+	cin >> n;
+	int *arr = new int[n];
+	cout << "Please enter your array:\n";
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	delete[] arr;
+
+	return 0;
+}
